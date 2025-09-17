6dde3378954d352bc71d8b66d7ddd3abb36b4ad1
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-17 15:21:23 -0300

    feat:aula1.

--- 17-09/atv2streamlit.py
+++ 17-09/atv2streamlit.py
@@ -4,3 +4,16 @@ os.system ("cls")
 tentativas = 0 
 login_salvo = "marta"
 senha_salva = "123"
+
+while True:
+    for i in range(3):
+        print(f"Tentativa: {i+1}")
+    login = input("Digite seu login: ")
+    senha = input("Digite sua senha: ")
+
+    if login == login_salvo and senha == senha_salva:
+        print("Bem-vindo")
+        break
+    else:
+        print("login ou senha ivalidos.\n")
+    break
