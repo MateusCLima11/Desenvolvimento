2c347926d51bf32daa9b05937404191178dd5814
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-03 15:01:30 -0300

    feat:aula2

--- Funcoes/funcoes1.py
+++ Funcoes/funcoes1.py
@@ -3,13 +3,16 @@ os.system("cls")
 
 #Funcao com passagem de parametros.
 #Criando uma funcao.
-def saudacao(nome, idade):
+def saudacao(nome, idade, altura, peso):
     print(f"Olá, {nome}! Bem-vindo(a)!")
-    print(f"Sua iadade é {idade} anos.")
+    print(f"Sua idade é {idade} anos.")
+    print(f"Sua altura é {altura}cm")
+    print(f"Seu peso é {peso}kg")
     
 print("Solicitando dados.")
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
-
+altura = int(input("Digite sua altura em cm: "))
+peso = float(input("Digite seu peso em KG:"))
 #Chamando a funcao.
-saudacao(nome, idade)
+saudacao(nome, idade, altura, peso)
