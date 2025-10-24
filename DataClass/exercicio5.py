0ea1bd0e8babaf93cf062fddfe25a46d721aaffc
MateusCLima11 <mateuslimasesi@gmail.com>
2025-10-24 14:44:13 -0300

    Add files via upload

--- DataClass/exercicio5.py
+++ DataClass/exercicio5.py
@@ -3,26 +3,32 @@ os.system("cls")
 from dataclasses import dataclass
 
 @dataclass
-class Pessoa:
+class Pessoas:
     nome: str
     cpf: str
     telefone: str
 
     def mostrar_dados(self):
+        print("MOSTRANDO DADOS")
         print(f"Nome: {self.nome}")
         print(f"CPF: {self.cpf}")
-        print(f"Telefoone: {self.telefone}")
+        print(f"Telefone: {self.telefone}")
 
     def dados_sms_marketing(self):
+        print("MOSTRAANDO TELEFONE")
         print(f"Telefone: {self.telefone}")
+
 
 lista_pessoas = []
 
 for i in range(3):
-    dados_pessoas = Pessoa(nome= input("Digite seu nome: "),
+    dados_pessoas = Pessoas(nome= input("Digite seu nome: "),
                            cpf= input("Digite seu CPF: "),
                            telefone= input("Digite se telefone: "))
-lista_pessoas.append(dados_pessoas)  
+    lista_pessoas.append(dados_pessoas)  
 
-for Pessoa in lista_pessoas:
-        dados_pessoas.mostrar_dados()
+import os
+os.system("cls")
+
+for dados_pessoas in lista_pessoas:
+    dados_pessoas.mostrar_dados()
