baf1dccd197d348be59c85c6ef2cb6ea90496f89
MateusCLima11 <mateuslimasesi@gmail.com>
2025-11-19 16:58:40 -0300

    Add files via upload

--- DataClass/dataclass2.py
+++ DataClass/dataclass2.py
@@ -1,3 +1,5 @@
+import os
+os.system("cls") 
 
 from dataclasses import dataclass
 
@@ -7,7 +9,7 @@ class Paciente:
     idade: int
 
     def exibir_dados(self):
-        print(f"Nome: {self.nome} /nIdade: {self.idade}")
+        print(f"Nome: {self.nome} /nIdade: {self.idade}\n")
 
 lista_de_pacientes = []
 QUANTIDADE_DE_PACIENTES = 2
@@ -27,12 +29,17 @@ for i in range(QUANTIDADE_DE_PACIENTES):
             print("Dados salvos com sucesso.")
 
 print("\nExibindo lista de pacientes: ")
+lista = []
 try:
     #"r" - read - leitura
     with open(nome_do_arquivo, "r") as arquivo:
         #Recebe todos os dados do arquivo de uma só vez.
         lista_todos_pacientes = arquivo.readlines()
         for paciente in lista_todos_pacientes:
-            print(f"- {paciente.strip()}")
+            nome, idade = paciente.strip().split(",")
+            dados_paciente = Paciente(nome=nome, idade=int(idade))
+            lista.append(dados_paciente)
+    for paciente in lista:
+        paciente.exibir_dados()
 except FileNotFoundError:
     print("O arquivo não foi encontrado.")
